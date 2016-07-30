9660908a9b850169fca3868ed5270ad3883ed35b
hihihot2 <17679@hanmail.net>
2016-07-30 12:10:04 +0900

    스플레쉬 화면 추가

---
+++ WearWether/app/src/main/java/app/com/wearwether/SplashActivity.java
@@ -0,0 +1,26 @@
+package app.com.wearwether;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import app.com.wearwether.R;
+/**
+ * Splash 화면 구성
+ * */
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        findViewById(R.id.splash_tv).postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(SplashActivity.this, MainActivity.class));
+                finish();
+            }
+        }, 1000);
+    }
+}
